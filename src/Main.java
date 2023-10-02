65ea05bc62bce029f2fbb0e77022e47667160f69
Lucas-Ramos <drawingworldab@gmail.com>
2023-10-02 10:43:28 -0300

    validaçao de cpf

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Turista mochileiro = new Turista("Supla");
         Turista mochileira = new Turista();
 
+        Scanner tcd = new Scanner(System.in);
+
+        String cpf = tcd.nextLine();
         // Definir forma de cada turista
 
-        mochileiro.setCpf(Validacao.cpf("124"));
+
+        if(Validacao.cpf(cpf)){
+            mochileiro.setCpf(cpf);
+            System.out.println("CPF valido");
+        }
+        else{
+            System.out.println("CPF Inválido!");
+        }
+        tcd.close();
+
 
         mochileira.setCpf("235");
         mochileira.setNome("namor");
